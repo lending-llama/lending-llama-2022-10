bfc0d6c0eb29f51a130b4c1a6563aaa431c5083b
Sebastian Ratkowski <ratkowski@sipgate.de>
2022-10-26 14:16:07 +0200

    refactor errors.spec.js
    Co-authored-by: Daniel Knerr <knerr@sipgate.de>
    Co-authored-by: Mathias Bethge <bethge@sipgate.de>

--- frontend/src/reducers/errors.spec.js
+++ frontend/src/reducers/errors.spec.js
@@ -1,15 +1,28 @@
-import {errorsReducer} from "./errors";
+import {errorsReducer, getFirstError} from "./errors";
 import {errorsAdded, errorsDismissedFirst} from "../actions/errors";
+import * as r from 'ramda'
 
 describe('Errors reducer', () => {
-  const state = ['foo']
+
+
   it('removes error', () => {
-    state.push('bar')
-    expect(errorsReducer(state, errorsDismissedFirst()))
-      .toEqual(['bar'])
+    const state = r.pipe(
+      () => errorsReducer(),
+      x => errorsReducer(x, errorsAdded("foo")),
+      x => errorsReducer(x, errorsAdded("bar")),
+      x => errorsReducer(x, errorsDismissedFirst())
+    )()
+
+    expect(getFirstError(state))
+      .toEqual("bar")
   })
   it('saves payloads', () => {
-    expect(errorsReducer(state, errorsAdded('baz')))
-      .toEqual(['foo', 'bar', 'baz'])
+    const state = r.pipe(
+      () => errorsReducer(),
+      x => errorsReducer(x, errorsAdded("foo")),
+      x => errorsReducer(x, errorsAdded("baz"))
+    )()
+    expect(state)
+      .toEqual(['foo', 'baz'])
   })
 })
