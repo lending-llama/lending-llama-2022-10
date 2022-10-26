7ceb08ac095dd791c3292d4595b1ba874dad56e5
Alex Bepple <alex@bepple.de>
2022-10-26 15:32:16 +0200

    decouple BetRate.sepc from rate formatting

--- frontend/src/BestRate.spec.js
+++ frontend/src/BestRate.spec.js
@@ -1,12 +1,12 @@
 import nock from 'nock'
 import {render, waitFor} from "@testing-library/react";
 import React from "react";
-import {App} from "./App";
 import {Provider} from "react-redux";
 import {store} from "./redux";
 import 'whatwg-fetch' // sets global.fetch
 import '@testing-library/jest-dom'
-import {BestRate} from "./BestRate"; // extends Jest with .toHaveTextContent
+import {BestRate} from "./BestRate";
+import {formatRate} from "./presentation/formatting";
 
 function getBestRateSurroundingDivId() {
   return 'allocation-c020b901';
@@ -16,16 +16,16 @@ describe('BestRate', () => {
   beforeEach(nock.cleanAll)
 
   it('displays best rate in UI', async () => {
-    const randomPlatformName = 'Random Platform Name';
-    const randomRate = 1;
+    const anyPlatformName = 'Random Platform Name';
+    const anyRate = 1;
 
     nock(/./)
       .get('/api/best-rate')
-      .reply(200, {name: randomPlatformName, rate: randomRate})
+      .reply(200, {name: anyPlatformName, rate: anyRate})
 
     const c = render(<Provider store={store}><BestRate/></Provider>);
-    await waitFor(() => c.getByText(randomPlatformName, {exact: false}))
+    await waitFor(() => c.getByText(anyPlatformName, {exact: false}))
 
-    expect(c.getByTestId(getBestRateSurroundingDivId())).toHaveTextContent('1.00%')
+    expect(c.getByTestId(getBestRateSurroundingDivId())).toHaveTextContent(formatRate(anyRate))
   })
 })
