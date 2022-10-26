415050d2c711cedf73cff24d9b421c8237f69ce3
Lennart Rosam <rosam@sipgate.de>
2022-10-26 14:00:29 +0200

    tests: make inputs explicit / mock server
    Co-authored-by: Robert Deppe <deppe@sipgate.de>

--- backend/src/test/java/com/example/AllocationControllerAllocationTest.java
+++ backend/src/test/java/com/example/AllocationControllerAllocationTest.java
@@ -1,37 +1,73 @@
 package com.example;
 
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.util.Arrays;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AllocationControllerAllocationTest {
+class AllocationControllerAllocationTest {
 
-	@Autowired
-	private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private MockRestServiceServer mockServer;
     private ObjectMapper mapper = new ObjectMapper();
 
+    @BeforeEach
+    public void beforeEach() {
+        mockServer = MockRestServiceServer.createServer(restTemplate);
+    }
+
     @Test
-	public void findsBest() throws Exception {
-        var expected = Arrays.asList(
-            new Allocation().setName("Ledn").setRate(6.25),
-            new Allocation().setName("BlockFi").setRate(4.5)
+    void findsBest() throws Exception {
+        final String anyPlatformName = "platform";
+        final double anyRate = 1.0;
+        final var anyPlaform = new Platform()
+            .setName(anyPlatformName)
+            .setTiers(new Platform.Tier[]{new Platform.Tier().setRate(anyRate)});
+
+        final String btcRatesUrl = "https://priceless-khorana-4dd263.netlify.app/btc-rates.json";
+        mockServer
+            .expect(
+                requestTo(new URI(btcRatesUrl)))
+            .andExpect(method(HttpMethod.GET))
+            .andRespond(withStatus(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(mapper.writeValueAsString(List.of(anyPlaform)))
+            );
+
+        final var expected = Collections.singletonList(
+            new Allocation().setName(anyPlatformName).setRate(anyRate)
         );
-        mvc.perform(MockMvcRequestBuilders.get("/allocations?amount=1.1")
-            .accept(MediaType.APPLICATION_JSON))
+
+        final String anyAmount = "1";
+        mvc.perform(MockMvcRequestBuilders.get("/allocations?amount=" + anyAmount)
+                .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().json(mapper.writeValueAsString(expected)));
-	}
+    }
 }
