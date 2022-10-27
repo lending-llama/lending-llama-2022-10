48be6a0d648eb2b62146ea33422e535867814ee2
Alex Bepple <alex@bepple.de>
2022-10-27 20:05:23 +0200

    !!! bring back rounded corners: Play CDN does not seem to like template strings

--- frontend/src/presentation/Panels.jsx
+++ frontend/src/presentation/Panels.jsx
@@ -4,12 +4,14 @@ import React from "react";
  cp. https://tailwindui.com/components/application-ui/layout/panels
  */
 
-export const Card = ({header, children}) =>
-  <div className={ `bg-white overflow-hidden shadow rounded-lg${header ? " divide-y divide-gray-200" : ""}`}>
+export const Card = ({header, children}) => {
+  const additionalClassesWithHeader = header ? " divide-y divide-gray-200" : "";
+  return <div className={'bg-white overflow-hidden shadow rounded-lg' + additionalClassesWithHeader}>
     {header ? <div className="px-4 py-5 sm:px-6">
       {header}
     </div> : null}
     <div className="px-4 py-5 sm:p-6">
       {children}
     </div>
-  </div>
+  </div>;
+}
